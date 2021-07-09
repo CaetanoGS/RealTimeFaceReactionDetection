97374a0c0f2d15fbbbe5b0ecd5b3bbb2fd7cff35
Gustavo Caetano de Souza <gustavocaetano100@gmail.com>
2021-07-09 23:48:32 +0200

    Problems with the request

--- src/static/js/script.js
+++ src/static/js/script.js
@@ -24,9 +24,38 @@ video.addEventListener('play', () => {
     const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
     const resizedDetections = faceapi.resizeResults(detections, displaySize)
     canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-    console.log(detections);
     faceapi.draw.drawDetections(canvas, resizedDetections)
     faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
     faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
-  }, 100)
+
+    if (detections[0].expressions) {
+      var myHeaders = new Headers();
+      myHeaders.append("Content-Type", "application/json");
+
+      var raw = JSON.stringify({
+        "local_name": "test",
+        "created_at": "2021-07-09T20:48:09.859650Z",
+        "angry": detections[0].expressions.angry,
+        "disgusted": detections[0].expressions.disgusted,
+        "fearful": detections[0].expressions.fearful,
+        "happy": detections[0].expressions.happy,
+        "neutral": detections[0].expressions.neutral,
+        "sad": detections[0].expressions.sad,
+        "surprised": detections[0].expressions.surprised
+      });
+
+      var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+      };
+
+      fetch("https://cors-vertra.herokuapp.com/http://localhost:8000/facial/", requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+    }
+
+  }, 1000)
 })
